826c49128dbaa90b0ddb0a6aa8912068a2ba8f4e
pl4tin3um <platineumuru@gmail.com>
2026-09-23 23:54:08 -0300

    error fix

--- Assets/js/common.js
+++ Assets/js/common.js
@@ -8,7 +8,6 @@ const LS_DRAFTS  = 'mma2_drafts';
 const LS_FORMS   = 'mma2_forms';
 const SS_SESSION = 'mma2_session';
 
-function $(sel, ctx) { return (ctx \vert{}\vert{} document).querySelector(sel); } function $$(sel, ctx) { return Array.from((ctx || document).querySelectorAll(sel)); }
 function uid(prefijo) { return prefijo + '_' + Date.now().toString(36) + Math.random().toString(36).slice(2,7); }
 
 function formatearFecha(iso) {
