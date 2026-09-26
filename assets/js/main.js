fff945b52e5a2087ceab69e50f455cb36a93c170
pl4tin3um <platineumuru@gmail.com>
2026-09-26 13:38:31 -0300

    direcciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,6 +187,6 @@ if(formLogin) {
     if(err) err.innerHTML = '';
     setSession({ id:encontrado.id, username:encontrado.username, role:encontrado.role, name:encontrado.name });
     formLogin.reset();
-    window.location.href = (encontrado.role === 'admin') ? 'Public/admin.html' : 'Public/editor.html';
+    window.location.href = (encontrado.role === 'admin') ? 'pages/admin.html' : 'pages/editor.html';
   });
 }
